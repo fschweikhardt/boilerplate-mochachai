423cc24ddb2f8870bc70dfb762af0f9fee4185ee
fschweikhardt <schweikhardt.web@gmail.com>
2021-07-22 21:53:49 -0400

    install nodemon

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 
 suite('Unit Tests', function () {
-  suite('Basic Assertions', function () {
+  suite.only('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
       assert.isNull(null, 'this is an optional error description - e.g. null is null');
@@ -10,20 +10,20 @@ suite('Unit Tests', function () {
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'a string is not undefined');
+      assert.isNull(null, 'null is not undefined');
+      assert.isNotNull(undefined, 'undefined IS undefined');
+      assert.isNotNull('hello', 'a string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'a string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isNull(null, 'null is falsey');
+      assert.isNotNull("I'm truthy", 'a string is truthy');
+      assert.isNotNull(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'double negation of a truthy is true');
+      assert.isNotNull(true, 'true is true');
+      assert.isNotNull(!!'double negation', 'double negation of a truthy is true');
       assert.fail({ value: 'truthy' }, 'A truthy object is NOT TRUE (neither is false...)');
     });
   });
