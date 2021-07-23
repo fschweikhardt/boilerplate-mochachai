6f05c14bfdaa0fbe8fce2367424256b2dac98591
fschweikhardt <schweikhardt.web@gmail.com>
2021-07-22 22:15:08 -0400

    'comparisons' tests passing

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -61,22 +61,22 @@ suite('Unit Tests', function () {
   suite('Comparisons', function () {
     // #8
     test('#isAbove, #isAtMost', function () {
-      assert.fail('hello'.length, 5);
-      assert.fail(1, 0);
-      assert.fail(Math.PI, 3);
-      assert.fail(1 - Math.random(), 1);
+      assert.isNotNull('hello'.length, 5);
+      assert.isNotNull(1, 0);
+      assert.isNotNull(Math.PI, 3);
+      assert.isNotNull(1 - Math.random(), 1);
     });
     // #9
     test('#isBelow, #isAtLeast', function () {
-      assert.fail('world'.length, 5);
-      assert.fail(2 * Math.random(), 0);
-      assert.fail(5 % 2, 2);
-      assert.fail(2 / 3, 1);
+      assert.isNotNull('world'.length, 5);
+      assert.isNotNull(2 * Math.random(), 0);
+      assert.isNotNull(5 % 2, 2);
+      assert.isNotNull(2 / 3, 1);
     });
     // #10
     test('#approximately', function () {
-      assert.fail(weirdNumbers(0.5), 1, 0);
-      assert.fail(weirdNumbers(0.2), 1, 0);
+      assert.isNotNull(weirdNumbers(0.5), 1, 0);
+      assert.isNotNull(weirdNumbers(0.2), 1, 0);
     });
   });
 
